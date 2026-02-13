4050c6a4dc68b4fc1fda59231cf82d0c1ff5b0bc
escandon100 <olakunle23odunade@gmail.com>
2026-02-13 15:32:41 +0100

    update

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-import express from "express";
-import cors from "cors"
-import dotenv from "dotenv";
-import mongoose from "mongoose";
-import adminLoginRoutes from './routes/adminLogin.js';
-import adminDashboardRoutes from './routes/adminDashboard.js'; 
-import userRegisterRoutes from "./routes/userRegister.js";
-import userLoginRoutes from "./routes/userLogin.js";
-import userDashboardRoutes from "./routes/userDashboard.js";
-import allUsersRoutes from "./routes/allUsers.js";
-import subscribeRoutes from "./routes/subscribe.js";
-import transactionRoutes from "./routes/transaction.js"
+  import express from "express";
+  import cors from "cors"
+  import dotenv from "dotenv";
+  import mongoose from "mongoose";
+  import adminLoginRoutes from './routes/adminLogin.js';
+  import adminDashboardRoutes from './routes/adminDashboard.js'; 
+  import userRegisterRoutes from "./routes/userRegister.js";
+  import userLoginRoutes from "./routes/userLogin.js";
+  import userDashboardRoutes from "./routes/userDashboard.js";
+  import allUsersRoutes from "./routes/allUsers.js";
+  import subscribeRoutes from "./routes/subscribe.js";
+  import transactionRoutes from "./routes/transaction.js"
 
-dotenv.config();
+  dotenv.config();
 
-const app = express()
+  const app = express()
 
-const MONGO_URI = process.env.MONGO_URI
+  const MONGO_URI = process.env.MONGO_URI
 
-mongoose.connect(MONGO_URI)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch(err => {
-    console.error("MongoDB connection error:", err);
-  });
-
-
-app.use(cors({ origin : "https://www.lyft-client.vercel.app" , credentials : true}));
-
-  
+  mongoose.connect(MONGO_URI)
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch(err => {
+      console.error("MongoDB connection error:", err);
+    });
 
 
-app.use(express.json());
+  app.use(cors({ origin : "https://lyft-client.vercel.app" , credentials : true}));
 
-app.use('/api/adminLogin', adminLoginRoutes);
-app.use('/api/adminDashboard', adminDashboardRoutes);
-app.use("/api/userRegister", userRegisterRoutes)
-app.use("/api/userLogin" , userLoginRoutes)
-app.use('/api/userDashboard', userDashboardRoutes);
-app.use('/api/allUsers' , allUsersRoutes);
-app.use("/api/subscribe" , subscribeRoutes)
-app.use("/api/transactions" , transactionRoutes)
+    
 
 
-app.listen(5000, () => console.log(`Server running on port 5000`));
+  app.use(express.json());
+
+  app.use('/api/adminLogin', adminLoginRoutes);
+  app.use('/api/adminDashboard', adminDashboardRoutes);
+  app.use("/api/userRegister", userRegisterRoutes)
+  app.use("/api/userLogin" , userLoginRoutes)
+  app.use('/api/userDashboard', userDashboardRoutes);
+  app.use('/api/allUsers' , allUsersRoutes);
+  app.use("/api/subscribe" , subscribeRoutes)
+  app.use("/api/transactions" , transactionRoutes)
+
+
+  app.listen(5000, () => console.log(`Server running on port 5000`));
 
 
 
